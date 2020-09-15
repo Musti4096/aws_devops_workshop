b5f05a5a4a75fe6b3edb29c07e333b6e6ce3a564
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-09-15 15:50:01 +0200

    updated

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -40,5 +40,5 @@ def index():
 
 #    app.run(host='0.0.0.0', port=80)
 if __name__ == "__main__":
-    app.run(debug = True)
-    #app.run(host="0.0.0.0", port=80)
+    #app.run(debug = True)
+    app.run(host="0.0.0.0", port=80)
