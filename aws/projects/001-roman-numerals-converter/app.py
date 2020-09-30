d50653c58872bdc19d55af02b8a5eb2f8af23aea
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-09-30 20:28:36 +0200

    roman num conv project files updated

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -1,36 +1,33 @@
-# Import Flask modules
-from flask import Flask, url_for, render_template, request
+from flask import Flask, render_template, request
 
-# Create an object named app
 app = Flask(__name__)
-
-def romenconvert(num):
-    num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
-           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
-    num=int(num)
-    if num>4000 or num<1:
-        return ("Not Valid Input !!!")
-    else :
-        roman = ''
-        while num > 0:
-            for i, r in num_map:
-                while num >= i:
-                    roman += r
-                    num -= i
-        return roman
-
-@app.route("/", methods = ["POST", "GET"])
-def index():
-    developer_name = "E2426 Mustafa"
-    if request.method == "POST":
-        number_decimal = request.form.get("number")
-        number_roman = romenconvert(number_decimal)
-        return render_template("result.html", number_decimal = number_decimal, number_roman = number_roman, developer_name = developer_name )
-    else:
-        return render_template("index.html", developer_name = developer_name)
+developer_name = "E2426 Mustafa"
 
 
-#    app.run(host='0.0.0.0', port=80)
+def convert(decimal_num):
+    roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+    num_to_roman = ''
+    for i in roman.keys():
+        num_to_roman += roman[i] * (decimal_num//i)
+        decimal_num %= i
+    return num_to_roman
+
+@app.route("/", methods=["GET"])
+def main_get():
+    return render_template("index.html", developer_name = developer_name, not_valid=False )
+
+@app.route("/", methods=["POST"])
+def main_post():
+    alpha = request.form["number"]
+    if not alpha.isdecimal():
+        return render_template("index.html", developer_name = developer_name, not_valid = True)
+    
+    number = int(alpha)
+    if not 0 < number < 4000:
+        return render_template("index.html", developer_name = developer_name, not_valid = True)
+        
+    return render_template("result.html", developer_name = developer_name, number_decimal= number , number_roman= convert(number) )
+
 if __name__ == "__main__":
-    #app.run(debug = True)
-    app.run(host="0.0.0.0", port=80)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0/0", port=80)
---
+++ python/coding-challenges/cc-004-validate-brackets-combination/validate_bracets.py
@@ -0,0 +1,14 @@
+def isValid(s):
+      bracket_map = {"(": ")", "[": "]",  "{": "}"}
+  open_par = set(["(", "[", "{"])
+  stack = []
+  for i in s:
+    if i in open_par:
+        stack.append(i)
+    elif stack and i == bracket_map[stack[-1]]:
+        stack.pop()
+    else:
+        return False
+  return stack == []
+combination = input('Write a string that contains only `(`, `)`, `{`, `}`, `[` and `]`: ')
+print(isValid(combination))
