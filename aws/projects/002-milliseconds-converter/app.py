995ba8714d07bed1543b5ff895992102ba251f55
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-10-07 17:34:41 +0200

    weekly upodate

--- aws/projects/002-milliseconds-converter/app.py
+++ aws/projects/002-milliseconds-converter/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+developer_name = "E2426 - Mustafa"
 def millisecond_converter(millisec):
     value = int(millisec)
     if value < 1000:
@@ -24,17 +24,22 @@ def millisecond_converter(millisec):
         elif second:
             return f"{second} second/s" 
 
-@app.route("/", methods=["POST", "GET"])
-def index():
-    developer_name = "E2426 Mustafa"
-    if request.method == "POST":
-        millisec = request.form.get("number")
-        result = millisecond_converter(millisec)
-        return render_template("result.html",milliseconds= millisec, result = result, developer_name = developer_name)
-    else:
-        return render_template("index.html", developer_name = developer_name)
+@app.route("/", methods=["GET"])
+def main_get():
+    return render_template("index.html", developer_name= developer_name, not_valid= False)
+
+@app.route("/", methods=["POST"])
+def main_post():
+    second = request.form["number"]
+    if not second.isdecimal():         return render_template("index.html", developer_name= developer_name, not_valid=True)
+    
+    second_int = int(second)
+    if not second_int > 0:
+        return render_template("index.html", developer_name= developer_name, not_valid=True)
+    
+    return render_template("result.html", developer_name= developer_name, milliseconds=second, result=millisecond_converter(second))
 
 #    app.run(host='0.0.0.0', port=80)
 if __name__ == "__main__":
-    #app.run(debug = True)
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug = True)
+    #app.run(host="0.0.0.0", port=80)
