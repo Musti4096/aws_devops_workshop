c8f842a51aee2bbb1c654fe41916bb506b0644c4
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-10-07 17:39:27 +0200

    weekly update

--- aws/projects/002-milliseconds-converter/app.py
+++ aws/projects/002-milliseconds-converter/app.py
@@ -41,5 +41,5 @@ def main_post():
 
 #    app.run(host='0.0.0.0', port=80)
 if __name__ == "__main__":
-    app.run(debug = True)
-    #app.run(host="0.0.0.0", port=80)
+    #app.run(debug = True)
+    app.run(host="0.0.0.0", port=80)
