fe21570c15dd8617a6d24e6181fcbd09f6982a1e
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-10-13 09:10:32 +0200

    phonebook project files added

--- aws/projects/004-phonebook-web-application/phonebook-app.py
+++ aws/projects/004-phonebook-web-application/phonebook-app.py
@@ -10,7 +10,7 @@ db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8')
 # Configure mysql database
 app.config['MYSQL_DATABASE_HOST'] = db_endpoint.readline().strip()
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'clarusway-1'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Aa123456'
 app.config['MYSQL_DATABASE_DB'] = 'phonebook'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 db_endpoint.close()
