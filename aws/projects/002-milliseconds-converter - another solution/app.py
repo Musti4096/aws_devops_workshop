96aa742c09c9a1bef2c63661e409b1c4b3b11e75
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-10-14 22:10:02 +0200

    millisecond conv files adde

--- aws/projects/002-milliseconds-converter - another solution/app.py
+++ aws/projects/002-milliseconds-converter - another solution/app.py
@@ -29,5 +29,5 @@ def main_post():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(host="0.0.0.0/0", port = 80)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0/0", port = 80)
