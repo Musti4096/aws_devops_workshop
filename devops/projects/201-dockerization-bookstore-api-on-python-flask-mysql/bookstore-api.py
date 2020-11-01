6ffe7c113b97d9fb62f08329a34c936bf51cebcd
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-11-01 08:57:41 +0100

    updated

--- devops/projects/201-dockerization-bookstore-api-on-python-flask-mysql/bookstore-api.py
+++ devops/projects/201-dockerization-bookstore-api-on-python-flask-mysql/bookstore-api.py
@@ -126,7 +126,7 @@ def remove_book(book):
 # and assign to the static route of ('/')
 @app.route('/')
 def home():
-    return "Welcome to Callahan's Bookstore API Service"
+    return "Welcome to E2426-Mustafa's Bookstore API Service"
 
 # Write a function named `get_books` which returns all books in JSON format for `GET`,
 # and assign to the static route of ('/books')
