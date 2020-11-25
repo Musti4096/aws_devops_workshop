563fea34c88c732ef78c6d94e69a198631a400ec
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-11-25 21:49:53 +0100

    updated

---
+++ devops/projects/202-a Docker Swarm Deployment of Phonebook Application (Python Flask) with MySQL/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE phonebook_db.phonebook(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    number VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO phonebook_db.phonebook (name, number)
+    VALUES
+        ("Callahan", "1234567890"),
+        ("Sergio Taco", "67854"),
+        ("Vincenzo Altobelli", "876543554");
